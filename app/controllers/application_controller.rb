4f33c9d9d5d34362339e9d0ebe13f4036b36db5d
ait <ait@ex.in>
2020-03-05 04:29:54 +0900

    setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # ========== 追記 ==========
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i(name username))
+  end
 end
